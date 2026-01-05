46f3c4d44a7f3ae95d3134f8a70915380d62a574
Jan Claes <janclaes21@gmail.com>
2026-01-05 20:22:52 +0100

    V1.0.0 Added colored bricks, NO BLOCKS YET

---
+++ src/main/java/net/minecraftweter/colorfulbricks/ColorfulBricksClient.java
@@ -0,0 +1,10 @@
+package net.minecraftweter.colorfulbricks;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ColorfulBricksClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
